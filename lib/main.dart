ebdb929b801f5009aa1d762a66913bf01ae38dcd
KhHadad <khbinyathrib@gmail.com>
2026-05-07 00:35:55 +0300

    Initial commit: Setup project folders

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // ضروري جداً لاستخدام kIsWeb
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// استدعاء واجهات زملائك (تأكد من مطابقة المسارات)
+import 'web_app/HomePageWeb.dart';
+import 'mobile_app/HomePageMobile.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gift Store',
+      debugShowCheckedModeBanner: false,
+      // هنا الفحص الذكي
+      home: kIsWeb
+          ? const HomePageWeb() // واجهة الويب لصاحب العمل
+          : const HomePageMobile(),     // واجهة الموبايل للمشترين
+    );
+  }
+}
---
+++ lib/web_app/HomePageWeb.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class HomePageWeb extends StatelessWidget {
+  const HomePageWeb({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Text('web'),
+    );;
+  }
+}
